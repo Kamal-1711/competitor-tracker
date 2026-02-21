4a54812133f765bfb0d0cbac67ce8c625ebbcda1
Kamal-1711 <kamalakannan1701@gmail.com>
2026-02-22 01:55:56 +0530

    debug: add global test route

---
+++ app/test-route/page.tsx
@@ -0,0 +1,7 @@
+export default function TestRoute() {
+    return (
+        <div className="p-20 bg-green-500 text-white text-9xl font-extrabold min-h-screen">
+            GLOBAL TEST ROUTE SUCCESS
+        </div>
+    );
+}
